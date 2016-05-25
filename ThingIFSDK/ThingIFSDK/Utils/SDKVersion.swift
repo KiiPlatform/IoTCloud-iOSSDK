cec16ceaebdc2dcd1d2f526c8e0fdedf52cdd5b3
rizakii <riza.alaudin@kii.com>
2016-05-25 15:09:19 +0900

    change to manual versioning.

--- ThingIFSDK/ThingIFSDK/Utils/SDKVersion.swift
+++ ThingIFSDK/ThingIFSDK/Utils/SDKVersion.swift
@@ -11,12 +11,12 @@ public class SDKVersion: NSObject {
     public static let sharedInstance = SDKVersion()
     /** Version of the Thing-IF SDK */
     public var versionString:String?
-    internal var kiiSDKHeader:String?
+    internal let kiiSDKHeader:String = "sn=it;sv=1.1.1;pv=\(UIDevice.currentDevice().systemVersion)"
     private override init() {
-        let b:NSBundle? = NSBundle.allFrameworks().filter{$0.bundleIdentifier == "Kii-Corporation.ThingIFSDK"}.first
-        versionString = b?.infoDictionary?["CFBundleShortVersionString"] as! String?
-        if let v = versionString {
-            kiiSDKHeader = "sn=it;sv=\(v);pv=\(UIDevice.currentDevice().systemVersion)"
-        }
+//        let b:NSBundle? = NSBundle.allFrameworks().filter{$0.bundleIdentifier == "Kii-Corporation.ThingIFSDK"}.first
+//        versionString = b?.infoDictionary?["CFBundleShortVersionString"] as! String?
+//        if let v = versionString {
+//            kiiSDKHeader = "sn=it;sv=\(v);pv=\(UIDevice.currentDevice().systemVersion)"
+//        }
     }
 }
