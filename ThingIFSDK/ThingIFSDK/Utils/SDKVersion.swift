39ee957dcdc8be2292fd11788c7fabf91d900a9a
naoki.akimoto <naoki.akimoto@muraoka-design.com>
2016-11-02 16:50:28 +0900

    test

--- ThingIFSDK/ThingIFSDK/Utils/SDKVersion.swift
+++ ThingIFSDK/ThingIFSDK/Utils/SDKVersion.swift
@@ -10,8 +10,8 @@ import UIKit
 open class SDKVersion: NSObject {
     open static let sharedInstance = SDKVersion()
     /** Version of the Thing-IF SDK */
-    open let versionString:String?
-    internal var kiiSDKHeader:String?
+    open let versionString:String!
+    internal var kiiSDKHeader:String!
     fileprivate override init() {
         let b:Bundle? = Bundle.allFrameworks.filter{$0.bundleIdentifier == "Kii-Corporation.ThingIFSDK"}.first
         versionString = b?.infoDictionary?["CFBundleShortVersionString"] as! String?
