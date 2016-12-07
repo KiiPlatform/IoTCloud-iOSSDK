4c995654881c0a0d6cfe4ebff484713edb8484b8
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2016-12-07 18:35:15 +0900

    Files to implement TraitThingIFAPI and its builder are added.

---
+++ ThingIFSDK/ThingIFSDK/Trait/TraitThingIFAPI.swift
@@ -0,0 +1,9 @@
+//
+//  TraitThingIFAPI.swift
+//  ThingIFSDK
+//
+//  Created on 2016/12/07.
+//  Copyright (c) 2016 Kii. All rights reserved.
+//
+
+import Foundation
---
+++ ThingIFSDK/ThingIFSDK/Trait/TraitThingIFAPIBuilder.swift
@@ -0,0 +1,9 @@
+//
+//  TraitThingIFAPIBuilder.swift
+//  ThingIFSDK
+//
+//  Created on 2016/12/07.
+//  Copyright (c) 2016 Kii. All rights reserved.
+//
+
+import Foundation
