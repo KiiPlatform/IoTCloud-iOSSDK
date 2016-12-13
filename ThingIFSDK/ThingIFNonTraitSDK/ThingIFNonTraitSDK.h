e5309790c43430df8ee895137eab8b74cdec8fbd
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2016-12-13 18:11:46 +0900

    I added 3 targets to make each frameworks.

---
+++ ThingIFSDK/ThingIFNonTraitSDK/ThingIFNonTraitSDK.h
@@ -0,0 +1,19 @@
+//
+//  ThingIFNonTraitSDK.h
+//  ThingIFNonTraitSDK
+//
+//  Created on 2016/12/13.
+//  Copyright (c) 2016 Kii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ThingIFNonTraitSDK.
+FOUNDATION_EXPORT double ThingIFNonTraitSDKVersionNumber;
+
+//! Project version string for ThingIFNonTraitSDK.
+FOUNDATION_EXPORT const unsigned char ThingIFNonTraitSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ThingIFNonTraitSDK/PublicHeader.h>
+
+
