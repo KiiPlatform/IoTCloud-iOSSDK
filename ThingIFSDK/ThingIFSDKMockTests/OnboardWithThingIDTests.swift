2ed6ebc641e24af44d447c2a5c1f86866b1e78ad
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2017-06-16 12:17:07 +0900

    Add ThingIFHTTPMockTestBase and OnboardWithThingIDTests.

---
+++ ThingIFSDK/ThingIFSDKMockTests/OnboardWithThingIDTests.swift
@@ -0,0 +1,22 @@
+//
+//  OnboardWithThingIDTests.swift
+//  ThingIFSDK
+//
+//  Created on 2017/06/16.
+//  Copyright (c) 2017 Kii. All rights reserved.
+//
+
+import XCTest
+@testable import ThingIF
+
+class OnboardWithThingIDTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+}
---
+++ ThingIFSDK/ThingIFSDKMockTests/ThingIFHTTPMockTestBase.swift
@@ -0,0 +1,22 @@
+//
+//  ThingIFHTTPMockTestBase.swift
+//  ThingIFSDK
+//
+//  Created on 2017/06/16.
+//  Copyright (c) 2017 Kii. All rights reserved.
+//
+
+import XCTest
+@testable import ThingIF
+
+class ThingIFHTTPMockTestBase: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+}
