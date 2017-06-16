19b395e01089303ab386ed4b9cff4d6352efae9a
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2017-06-16 13:28:32 +0900

    Replace onboarding with thing id tests.

--- ThingIFSDK/ThingIFSDKMockTests/OnboardWithThingIDTests.swift
+++ ThingIFSDK/ThingIFSDKMockTests/OnboardWithThingIDTests.swift
@@ -41,4 +41,148 @@ class OnboardWithThingIDTests: ThingIFHTTPMockTestBase {
         }
         XCTAssertTrue(api.onboarded)
     }
+
+    func testOnboardWithThingIDByOwner403Error() throws {
+        let api = createThingIFAPI("test-onboard-with-id-403")
+        executeAsynchronous { expectation in
+            api.onboardWith(
+              thingID: "thing-id",
+              thingPassword: "thing-password") { target, error in
+                XCTAssertNil(target)
+                XCTAssertEqual(
+                  error,
+                  ThingIFError.errorResponse(
+                    required: ErrorResponse(403,
+                                            errorCode: "",
+                                            errorMessage: "")))
+                expectation.fulfill()
+            }
+        }
+    }
+
+    func testOnboardWithThingIDByOwner404Error() throws {
+        let api = createThingIFAPI("test-onboard-with-id-404")
+        executeAsynchronous { expectation in
+            api.onboardWith(
+              thingID: "thing-id",
+              thingPassword: "thing-password") { target, error in
+                XCTAssertNil(target)
+                XCTAssertEqual(
+                  error,
+                  ThingIFError.errorResponse(
+                    required: ErrorResponse(404,
+                                            errorCode: "",
+                                            errorMessage: "")))
+                expectation.fulfill()
+            }
+        }
+    }
+
+    func testOnboardWithThingIDByOwner500Error() throws {
+        let api = createThingIFAPI("test-onboard-with-id-500")
+        executeAsynchronous { expectation in
+            api.onboardWith(
+              thingID: "thing-id",
+              thingPassword: "thing-password") { target, error in
+                XCTAssertNil(target)
+                XCTAssertEqual(
+                  error,
+                  ThingIFError.errorResponse(
+                    required: ErrorResponse(500,
+                                            errorCode: "",
+                                            errorMessage: "")))
+                expectation.fulfill()
+            }
+        }
+    }
+
+    func testOnboardWithThingIDAndOptions() throws {
+        let api = createThingIFAPI("test-onboard-with-id-option-success")
+        XCTAssertFalse(api.onboarded)
+        executeAsynchronous { expectation in
+            api.onboardWith(
+              thingID: "thing-id",
+              thingPassword: "thing-password",
+              options: OnboardWithThingIDOptions(.standalone)) {
+                target, error in
+
+                XCTAssertNil(error)
+                XCTAssertNotNil(target)
+                if let target = target {
+                    XCTAssertEqual(
+                      StandaloneThingWrapper(target as? StandaloneThing),
+                      StandaloneThingWrapper(StandaloneThing(
+                                               "thing-id",
+                                               vendorThingID: "",
+                                               accessToken: "accesstoken")))
+                }
+                expectation.fulfill()
+            }
+        }
+        XCTAssertTrue(api.onboarded)
+    }
+
+    func testOnboardWithThingIDAndOptions403Error() throws {
+        let api = createThingIFAPI("test-onboard-with-id-option-403")
+        executeAsynchronous { expectation in
+            api.onboardWith(
+              thingID: "thing-id",
+              thingPassword: "thing-password",
+              options: OnboardWithThingIDOptions(.standalone)) {
+                target, error in
+
+                XCTAssertNil(target)
+                XCTAssertEqual(
+                  error,
+                  ThingIFError.errorResponse(
+                    required: ErrorResponse(403,
+                                            errorCode: "",
+                                            errorMessage: "")))
+                expectation.fulfill()
+            }
+        }
+    }
+
+    func testOnboardWithThingIDAndOptions404Error() throws {
+        let api = createThingIFAPI("test-onboard-with-id-option-404")
+        executeAsynchronous { expectation in
+            api.onboardWith(
+              thingID: "thing-id",
+              thingPassword: "thing-password",
+              options: OnboardWithThingIDOptions(.standalone)) {
+                target, error in
+
+                XCTAssertNil(target)
+                XCTAssertEqual(
+                  error,
+                  ThingIFError.errorResponse(
+                    required: ErrorResponse(404,
+                                            errorCode: "",
+                                            errorMessage: "")))
+                expectation.fulfill()
+            }
+        }
+    }
+
+    func testOnboardWithThingIDAndOptions500Error() throws {
+        let api = createThingIFAPI("test-onboard-with-id-option-500")
+        executeAsynchronous { expectation in
+            api.onboardWith(
+              thingID: "thing-id",
+              thingPassword: "thing-password",
+              options: OnboardWithThingIDOptions(.standalone)) {
+                target, error in
+
+                XCTAssertNil(target)
+                XCTAssertEqual(
+                  error,
+                  ThingIFError.errorResponse(
+                    required: ErrorResponse(500,
+                                            errorCode: "",
+                                            errorMessage: "")))
+                expectation.fulfill()
+            }
+        }
+    }
+
 }
